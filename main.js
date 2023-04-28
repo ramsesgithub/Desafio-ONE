09b94e4389412153e843bcf76413e47e45c3a2c7
Ramses <127460936+ramsesgithub@users.noreply.github.com>
2023-04-28 17:17:41 -0300

    responsive

--- main.js
+++ main.js
@@ -2,80 +2,62 @@ const botonEncriptar=document.getElementById("encriptar");
 const botonDesencriptar=document.getElementById("desencriptar");    
 const botonCopiar=document.getElementById("copiar");    
 
-
+const caracteresValidos = /^[a-zñ\s\¿\?\¡\!\,\.]+$/;
 
 botonEncriptar.addEventListener("click", myFunctionEncriptar);
-
 botonDesencriptar.addEventListener("click", myFunctionDesencriptar);
-
 botonCopiar.addEventListener("click", copiar);
-
-// function encriptar(){
-//     let texto=document.getElementById("textoEncriptar").value.toLowerCase();
-
-//     let encriptado=texto.replace(/e/mg, "enter");
-//     encriptado=encriptado.replace(/o/mg, "ober");
-//     encriptado=encriptado.replace(/i/mg, "imes");
-//     encriptado=encriptado.replace(/a/mg, "ai");
-//     encriptado=encriptado.replace(/u/mg, "ufat");
-    
-//     document.getElementById('textoDesencriptado').innerHTML=encriptado;
-// }
-
-// function desencriptar(){
-//     let texto=document.getElementById("textoEncriptar").value.toLowerCase();
-//     let encriptado=texto.replace(/enter/mg, "e");
-//     encriptado=encriptado.replace(/ober/mg, "o");
-//     encriptado=encriptado.replace(/imes/mg, "i");
-//     encriptado=encriptado.replace(/ai/mg, "a");
-//     encriptado=encriptado.replace(/ufat/mg, "u");
-    
-//     document.getElementById('textoDesencriptado').innerHTML=encriptado;
-// }
-
 
 
 
 function myFunctionEncriptar() {
-    let texto=document.getElementById("textoEncriptar").value.toLowerCase();
-    let encriptado=texto.replace(/e/mg, "enter");
-    encriptado=encriptado.replace(/o/mg, "ober");
-    encriptado=encriptado.replace(/i/mg, "imes");
-    encriptado=encriptado.replace(/a/mg, "ai");
-    encriptado=encriptado.replace(/u/mg, "ufat");
-    
-    document.getElementById('textoDesencriptado').innerHTML=encriptado;
+    let texto=document.getElementById("textoEncriptar").value;
+    if(caracteresValidos.test(texto)){
+        let encriptado=texto.replace(/e/mg, "enter");
+        encriptado=encriptado.replace(/o/mg, "ober");
+        encriptado=encriptado.replace(/i/mg, "imes");
+        encriptado=encriptado.replace(/a/mg, "ai");
+        encriptado=encriptado.replace(/u/mg, "ufat");
+        document.getElementById('textoDesencriptado').innerHTML=encriptado;
 
-    let x = document.getElementById('textoDesencriptado');
-    let c= document.getElementById("copiar");
-
-    if(document.getElementById("textoEncriptar").value!==""){
-        x.style.display = 'block';
-        c.style.display = 'block';
+        let x = document.getElementById('textoDesencriptado');
+        let c= document.getElementById("copiar");
+        if(document.getElementById("textoEncriptar").value!==""){
+            x.style.display = 'block';
+            c.style.display = 'block';
+        }else{
+            x.style.display = 'none';
+            c.style.display = 'none';
+        }
     }else{
-        x.style.display = 'none';
-        c.style.display = 'none';
+        alert("El texto no es valido, escribir en minusculas, sin acentos y sin números.");
     }
 }
 
-function myFunctionDesencriptar(){
-    let texto=document.getElementById("textoEncriptar").value.toLowerCase();
-    let encriptado=texto.replace(/enter/mg, "e");
-    encriptado=encriptado.replace(/ober/mg, "o");
-    encriptado=encriptado.replace(/imes/mg, "i");
-    encriptado=encriptado.replace(/ai/mg, "a");
-    encriptado=encriptado.replace(/ufat/mg, "u");
-    
-    document.getElementById('textoDesencriptado').innerHTML=encriptado;
 
-    let x = document.getElementById('textoDesencriptado');
-    let c= document.getElementById("copiar");
-    if(document.getElementById("textoEncriptar").value!==""){
-        x.style.display = 'block';
-        c.style.display = 'block';
+
+
+function myFunctionDesencriptar(){
+    let texto=document.getElementById("textoEncriptar").value;
+    if(caracteresValidos.test(texto)){
+        let encriptado=texto.replace(/enter/mg, "e");
+        encriptado=encriptado.replace(/ober/mg, "o");
+        encriptado=encriptado.replace(/imes/mg, "i");
+        encriptado=encriptado.replace(/ai/mg, "a");
+        encriptado=encriptado.replace(/ufat/mg, "u");
+        document.getElementById('textoDesencriptado').innerHTML=encriptado;
+        let x = document.getElementById('textoDesencriptado');
+        let c= document.getElementById("copiar");
+        
+        if(document.getElementById("textoEncriptar").value!==""){
+            x.style.display = 'block';
+            c.style.display = 'block';
+        }else{
+            x.style.display = 'none';
+            c.style.display = 'none';
+        }
     }else{
-        x.style.display = 'none';
-        c.style.display = 'none';
+        alert("El texto no es valido, escribir en minusculas, sin acentos y sin números.");
     }
 }
 
@@ -87,4 +69,5 @@ function copiar(){
     document.body.appendChild(copiado);
     copiado.select();
     document.execCommand("copy");
+    document.body.removeChild(copiado);
 }
